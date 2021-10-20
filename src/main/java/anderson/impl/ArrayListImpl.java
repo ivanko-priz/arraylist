987c48d6fb7f455809e10c9ca69ee04383eba6b2
Ivan Prizov <ivanko.priz@gamil.com>
2021-10-20 21:19:23 +0300

    Add crude implementation for ArrayList

---
+++ src/main/java/anderson/impl/ArrayListImpl.java
@@ -0,0 +1,50 @@
+package anderson.impl;
+
+import anderson.api.ArrayList;
+
+public class ArrayListImpl<E> implements ArrayList<E> {
+    @Override
+    public boolean add(E element) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+}
